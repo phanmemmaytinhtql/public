546bc8e16871ee9d01247cd3b51a9d5453938166
ProgrammingForFun <phanmemmaytinhtql@gmail.com>
2021-04-28 14:26:51 +0700

    Remove some files

--- HUST/Machine_learning/Documents/websites/How Naive Bayes Classifiers Work – with Python Code Examples_files/main(1).js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const esMain = {
-  'algolia-index-name': 'news-es',
-  'learn-to-code-cta-url': 'https://www.freecodecamp.org/espanol/learn/',
-  'forum-url': 'https://forum.freecodecamp.org/c/espanol/522',
-  'donate-url': 'https://www.freecodecamp.org/espanol/donate/',
-  'banner-url': 'https://chat.freecodecamp.org/home'
-};
